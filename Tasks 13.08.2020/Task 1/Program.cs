93e8ac1a989159fe0c2f12a1a2995aafb3c03979
Torelina <nataneri@mail.ru>
2020-08-13 14:17:05 +0300

    tasks1 13/08/2020

---
+++ Tasks 13.08.2020/Task 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите свой рост: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            int c = 100;
+            int weight = height - c;
+            Console.WriteLine("Ваш идеальный вес: {0}", weight );
+            Console.ReadKey(); 
+
+
+
+        }
+    }
+}
