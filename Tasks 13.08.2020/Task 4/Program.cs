9eae5d840be0b69423eddc3ae86e280745d39580
Torelina <nataneri@mail.ru>
2020-08-13 16:28:34 +0300

    Добавлено решение задачи 4

---
+++ Tasks 13.08.2020/Task 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите расстояние(км) от школы до стадиона: ");
+            double S = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите среднюю скорость(км/ч) движения: ");
+            double V = Convert.ToDouble(Console.ReadLine());
+            double T = (S / V) * 60;
+            Console.WriteLine("Время в минутах, которое затратит школьник на дорогу от школы до стадиона: {0}", T);
+            Console.ReadKey();
+        }
+    }
+}
